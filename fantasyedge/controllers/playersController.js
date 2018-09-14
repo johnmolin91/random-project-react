5bb1fe087dc09aa2c486fab80745d07b3b2012ca
johnmolin91 <john.molin91@gmail.com>
2018-09-14 14:02:32 -0400

    mid

--- fantasyedge/controllers/playersController.js
+++ fantasyedge/controllers/playersController.js
@@ -1,5 +1,14 @@
 const db = require("../models");
 
-db.playersadvanced.findAll( {limit: 10 }).then(function(dbNames) {
-    console.log(dbNames);
-})
+module.exports = {
+    findAll: function(req, res) {
+        db.playersadvanced
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+}
+
+// db.playersadvanced.findAll( {limit: 10 }).then(function(dbNames) {
+//     console.log(dbNames);
+// })
--- fantasyedge/routes/api/players.js
+++ fantasyedge/routes/api/players.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const playersController = require("../../controllers/playersController");
 
+// Matches with "/api/players"
 router.route("/")
     .post(playersController.findAll);
 
